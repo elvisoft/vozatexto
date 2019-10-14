c6af835e20704e025445ee9e002c24ac2a75b8f0
Elvis Mancilla <elvisoftbeta@gmail.com>
2019-10-14 19:23:41 -0300

    Update index.php

--- index.php
+++ index.php
@@ -17,20 +17,14 @@
 			color:red;
 			box-shadow: 0px 0px 12px #ccc;
 		}
-	</style>
-	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">  	
-  	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-  	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>  		
+	</style>			
 
 </head>
 <body>
 	<h1>Pasar voz a texto: www.webgratis.com.ar</h1>
-	<div id="text">
-	</div>
+	<div id="text">	
 	
-	<script type="text/javascript" src="js/funcion.js"></script>
-
-	<div id="dialog"  >
+	<script type="text/javascript" src="js/funcion.js"></script>	
 		
 	</div>
 </body>
